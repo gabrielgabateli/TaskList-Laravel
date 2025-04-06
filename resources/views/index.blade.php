fb505754625fc522e9eec02864501be63991a81c
Gabriel <gabateli16@gmail.com>
2025-04-06 16:55:34 -0300

    refactor: Creating/editing tasks
    Creating and editing tasks in a Laravel way

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
 <div>
         @forelse ($tasks as $task)
             <div>
-                <a href="{{ route("tasks.show", ["id" => $task->id]) }}">{{$task->title}}</a><br>
+                <a href="{{ route("tasks.show", ["task" => $task->id]) }}">{{$task->title}}</a><br>
             </div><br>
         @empty
             <div>There are no tasks!</div>
